2e6bc6aeae263ac69871163f4b928760fe3209a3
Rachel Myers <rm0909685@otc.edu>
2020-04-20 14:22:31 -0500

    Tab functionality is now working. Will fix styles in a future push.

--- LogViewerProject/wwwroot/js/site.js
+++ LogViewerProject/wwwroot/js/site.js
@@ -4,6 +4,7 @@
 var files;
 var fileContents = document.getElementById("fileContents");
 var fileList = document.getElementById("fileList");
+var displayFileContent = document.getElementById('displayFileContent');
 
 
 
@@ -24,44 +25,9 @@ FILE TREE FUNCTIONALITY
 ****/
 
 
-/** OLD CODE
-$(document).ready(function () {
-    $("#btOpenFile").on("change", fileInputControlChangeEventHandler);
-});
-
-
-
- 
-document.getElementById("filepicker").addEventListener("change", function (event) {
-    let output = document.getElementById("listing");
-    let files = event.target.files;
-    let fileContents = event.target.result;
-    let fileReader = new FileReader();
-
-    for (let i = 0; i < files.length; i++) {
-        let item = document.createElement("li");
-        item.innerHTML = files[i].webkitRelativePath;
-        output.appendChild(item);
-        $("#preview").text(fileContents); 
-        fileReader.readAsText(files[i]);
-    };
-}, false);
-
-function fileInputControlChangeEventHandler(event) {
-    let fileReader = new FileReader();
-    fileReader.onload = function (event) {
-        $("#preview").text(fileContents); 
-    }
-    fileReader.readAsText(files[0]);
-}
-**/
-
-
-//This function creates a check box for reach file and creates a label that's the filename
 
 // Check for the various File API support.
 if (window.File && window.FileReader && window.FileList && window.Blob) {
-    // Great success! All the File APIs are supported.
 } else {
     alert('The File APIs are not fully supported in this browser.');
 }
@@ -70,6 +36,10 @@ if (window.File && window.FileReader && window.FileList && window.Blob) {
 
 function handleFileSelect(evt) {
     files = evt.target.files; // FileList object
+
+    //clear any previously selected files
+    document.getElementById('fileList').innerHTML = '';
+  //  document.getElementById('tabContainer').innerHTML = '';
 
     // files is a FileList of File objects. List some properties.
     var output = [];
@@ -80,11 +50,14 @@ function handleFileSelect(evt) {
             f.lastModifiedDate ? f.lastModifiedDate.toLocaleDateString() : 'n/a'
         );
 
+    
+
+ 
 
         var chk = document.createElement('input');  // CREATE CHECK BOX.
 
-        chk.setAttribute('type', 'checkbox');       // SPECIFY THE TYPE OF ELEMENT.
-        chk.setAttribute('id', i);     // SET UNIQUE ID.
+        chk.setAttribute('type', 'checkbox');     
+        chk.setAttribute('id', i);     
         chk.setAttribute('value', i);
         chk.setAttribute('name', 'products');
 
@@ -102,18 +75,8 @@ function handleFileSelect(evt) {
 
         //Display the Versions file
         if (files[i].name == 'Versions.txt') {
-            var reader = new FileReader();
-            reader.onload = function (event) {
-                var contents = event.target.result;
-                document.getElementById("versions").innerHTML = contents;
-            };
-
-            reader.onerror = function (event) {
-                console.error("File could not be read! Code " + event.target.error.code);
-            };
-
-            reader.readAsText(files[i]);
-        } //end of display Versions file
+            document.getElementById(this.id).checked = true;
+        } 
     }
 
 }
@@ -126,64 +89,41 @@ document.getElementById('files').addEventListener('change', handleFileSelect, fa
 $(document).on('change', '[type=checkbox]', function (event) {
 
     var fileType = $(':checkbox').val().split('.').pop();
-    var tabContent = document.createElement('div');
 
     //read and display file if check box is selected
 
     if (event.target.checked) {
         var reader = new FileReader();
+        
+        var btTabName = document.createElement('input');
 
-
-        //1. create li and append to ul id = nav nav-pills
-        var tabName = document.createElement('li');
-        document.querySelector('ul').appendChild(tabName);
-
-        //a. id = "li" + files[(this.id)];
-        tabName.setAttribute('id', 'li' + files[this.id]);
-
-        //2. creat a inside of li element and append to li id
-        var tabLink = document.createElement('a');
-        tabName.appendChild(tabLink);
-
-        //a. innerHTML = this.name
-        tabLink.innerHTML = files[this.id].name;
-
-        //b. class = "active"
-        tabName.setAttribute('class', 'active');
-
-        //c. href= "div" + files[(this.id)];	
-        tabLink.setAttribute('href', '#div' + files[this.id]);
-
-        //d. data-toggle="pill"
-        tabLink.setAttribute('data-toggle', 'pill');
+        btTabName.setAttribute('class', 'tablinks');
+        btTabName.setAttribute('id', 'bt' + this.id);
+        btTabName.setAttribute('onclick', 'openFile(event)');
+        btTabName.setAttribute('type', 'button');
+        btTabName.setAttribute('value', files[this.id].name);
+        document.getElementById('navBar').append(btTabName);
 
         var tabClose = document.createElement('span');
         tabClose.innerHTML = ' X';
         tabClose.setAttribute('onclick', 'this.parentElement.style.display="none"');
-        tabLink.insertAdjacentHTML('afterend', tabClose);
-
-        //3. create a div and append to div id="tabContainer"
-        document.getElementById('displayContent').appendChild(tabContent);
-
-        //a. id="div" + files[(this.id)]
-        tabContent.setAttribute('div', 'li' + files[this.id]);
-
-        //b. class="tab-pane fade in active"
-        tabContent.setAttribute('class', 'tab-pane fade in active');
-
-        //c. innerHTML = file contents
+     
+        
 
         reader.onload = function (event) {
-            tabContent.style.visibility = 'visible';
+            
             var contents = event.target.result;
-            tabContent.innerHTML = contents;
+            displayFileContent.innerHTML = contents;
+           
         };
 
         reader.onerror = function (event) {
             console.error("File could not be read! Code " + event.target.error.code);
         };
 
-        reader.readAsDataURL(files[this.id]);
+        reader.readAsText(files[this.id]);
+      //  document.getElementById('testDiv').appendChild(tabContent);
+      
 
         /**if(files[this.id].type.match('image.*')) {
             reader.readAsDataURL(files[this.id]);
@@ -194,25 +134,12 @@ $(document).on('change', '[type=checkbox]', function (event) {
         //Hide content when checkbox is cleared
     } else if (!event.target.checked) {
 
-        tabContent.style.visibility = 'hidden';
+        //tabContent.style.visibility = 'hidden';
 
     }
 
 
 });
-
-
-/****
- * DISPLAY FILE INFORMATION
- * code source: codexworld.com/how-to/get-file-info-name-type-javascript/
- * ***/
-
-function fileInfo() {
-    var fileName = document.getElementByID('filepicker').files[0].name;
-    var fileSize = document.getElementByID('filepicker').files[0].size;
-    var fileType = document.getElementByID('filepicker').files[0].type;
-    var fileModifiedDate = document.getElementByID('filepicker').files[0].lastModifiedDate;
-}
 
 
 /****
@@ -231,14 +158,46 @@ searchTextEntered.addEventListener("focus", function () {
 
 TABS THAT DISPLAY FILE CONTENT FUNCTIONALITY
 
+
 ****/
 
-function openCity(cityName) {
-    var i;
-    var x = document.getElementsByClassName("city");
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
+function openFile(evt) {
+    var i, tablinks;
+
+    var btID = evt.currentTarget.id;
+    btID = btID.slice(2);
+    console.log(btID);
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
-    document.getElementById(cityName).style.display = "block";
+
+
+
+    var reader = new FileReader();
+    reader.onload = function (event) {
+       // fileContents.style.visibility = 'visible';
+        var contents = event.target.result;
+        displayFileContent.innerHTML = contents;
+    };
+
+    reader.onerror = function (event) {
+        console.error("File could not be read! Code " + event.target.error.code);
+    };
+
+    /**if(files[this.id].type.match('image.*')) {
+        reader.readAsDataURL(files[this.id]);
+    } else {
+        reader.readAsText(files[this.id]);
+    }**/
+
+    reader.readAsText(files[btID]);
+
+
+
+    
+   // tabContent.style.display = "block";
+   // displayFileContent.innerHTML = files[btID];
+    evt.currentTarget.className += " active"; 
 }
 
